35adb2ef6859821f7543d1db21bdab9a3a214ddf
codethi <docencia.thiago@gmail.com>
2022-03-17 16:13:01 -0300

    footer and page style

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import "./Nav.css";
+import { Link } from "react-router-dom";
+
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+import "./Home.css";
+
+export function Home() {
+  return <h1>Home</h1>;
+}
